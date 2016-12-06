103aa413fa5cf2a49c5dbcde6a16bd5d6afd572f
zino-PC\zino <zino1187@gmail.com>
2016-12-06 11:48:31 +0900

    a

--- app/src/main/java/com/solu/asyncclient/MainActivity.java
+++ app/src/main/java/com/solu/asyncclient/MainActivity.java
@@ -3,12 +3,16 @@ package com.solu.asyncclient;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     ViewPager viewPager;
     MyPagerAdapter myPagerAdapter;
-
+    Toolbar toolbar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +20,20 @@ public class MainActivity extends AppCompatActivity {
         viewPager=(ViewPager) findViewById(R.id.viewPager);
         myPagerAdapter=new MyPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_navi, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "눌름", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
 
